fade9e94026f5e08ee643489d959c889bf7d49c2
[Abdul] <abdul.osman997@gmail.com>
2019-10-18 00:52:27 -0700

    Code to load player image

--- Cross_RPG_Game.py
+++ Cross_RPG_Game.py
@@ -26,6 +26,10 @@ game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 game_screen.fill(WHITE)
 pygame.display.set_caption(SCREEN_TITLE)
 
+# Load player image from file directory
+player_image = pygame.image.load('player.png')
+player_image = pygame.transform.scale(player_image, (50, 50))
+
 #Main game loop
 while not is_game_over:
     
@@ -37,6 +41,12 @@ while not is_game_over:
             is_game_over = True
         print(event)
         
+        #pygame.draw.rect(game_screen, BLACK, [350, 350, 100, 100])
+        #pygame.draw.circle(game_screen, BLACK, (400, 300), 50)
+
+        # Draw the player image on top of the screen at (x, y) position
+        game_screen.blit(player_image, (375, 375))
+
         # Update graphics
         pygame.display.update()
         clock.tick(TICK_RATE)
