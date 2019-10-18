f00886718ab373baf706818dec2ad8ce14b54316
[Abdul] <abdul.osman997@gmail.com>
2019-10-18 01:02:29 -0700

    Update Cross_RPG_Game.py
    created a game class

--- Cross_RPG_Game.py
+++ Cross_RPG_Game.py
@@ -4,53 +4,63 @@
 
 import pygame
 
-pygame.init()
-
 # Screen Settings
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-
 SCREEN_TITLE = 'Crossy RPG'
-
-# Clock used to update game events and frames
-clock = pygame.time.Clock()
-TICK_RATE = 60 #Rate of 60 like FPS
-is_game_over = False
 
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-# Creates window of specified size and set the game window color
-game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-game_screen.fill(WHITE)
-pygame.display.set_caption(SCREEN_TITLE)
+# Clock used to update game events and frames
+clock = pygame.time.Clock()
+pygame.font.init()
+font = pygame.font.SysFont('comicsans', 75)
 
-# Load player image from file directory
-player_image = pygame.image.load('player.png')
-# Scale the image up
-player_image = pygame.transform.scale(player_image, (50, 50))
-
-#Main game loop
-while not is_game_over:
+class Game:
+    TICK_RATE = 60 #Rate of 60 like FPS
     
-    # Events usually mouse movement, clicks, exits
-    for event in pygame.event.get():
+    # Constructor for game class w/ width, height, and title
+    def __init__(self, title, width, height):
+        self.title = title
+        self.width = width
+        self.height = height
 
-        # if quit event, end the game
-        if event.type == pygame.QUIT:
-            is_game_over = True
-        print(event)
-        
-        #pygame.draw.rect(game_screen, BLACK, [350, 350, 100, 100])
-        #pygame.draw.circle(game_screen, BLACK, (400, 300), 50)
+        # Creates window of specified size and set the game window color
+        self.game_screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.game_screen.fill(WHITE)
+        pygame.display.set_caption(SCREEN_TITLE)
 
-        # Draw the player image on top of the screen at (x, y) position
-        game_screen.blit(player_image, (375, 375))
+    def run_game_loop(self):
+        is_game_over = False
 
-        # Update graphics
-        pygame.display.update()
-        clock.tick(TICK_RATE)
+        #Main game loop
+        while not is_game_over:
+            
+            # Events usually mouse movement, clicks, exits
+            for event in pygame.event.get():
+
+                # if quit event, end the game
+                if event.type == pygame.QUIT:
+                    is_game_over = True
+
+                print(event)
+                
+                #pygame.draw.rect(game_screen, BLACK, [350, 350, 100, 100])
+                #pygame.draw.circle(game_screen, BLACK, (400, 300), 50)
+
+                # Draw the player image on top of the screen at (x, y) position
+                # game_screen.blit(player_image, (375, 375))
+
+                # Update graphics
+                pygame.display.update()
+                clock.tick(TICK_RATE)
+
+pygame.init()
+
+new_game = Game(SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT)
+new_game.run_game_loop()
 
 # Quit pygame and the program
 pygame.quit()
